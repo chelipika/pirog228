19de618fcb88ed263ca492facc12b8204b96274b
chelipika <103322024+chelipika@users.noreply.github.com>
2024-11-17 17:30:04 +0500

    First version, using selenium via browser

---
+++ logins.py
@@ -0,0 +1,10 @@
+loggins = [
+    {
+        'login': "zhasurpirmatov",
+        'password': "7*2iYQFq@M+zFRq"
+    },
+    {
+        'login': 'barnokhonmetinova',
+        'password': '123456#'
+    }
+]
---
+++ multiple.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from logins import loggins
+driver = webdriver.Chrome()
+driver.get("https://login.emaktab.uz")
+for acc in loggins:
+    elem = driver.find_element(By.NAME, "login")
+    elem.clear()
+    elem.send_keys(acc['login'])
+    elem.send_keys(Keys.RETURN)
+    elem2 = driver.find_element(By.NAME, "password")
+    elem2.clear()
+    elem2.send_keys(acc['password'])
+    elem2.send_keys(Keys.RETURN)
+    elem3 = driver.find_element(By.NAME, "logout")
+    elem3.click()
+    
+
+driver.close()
+
+
+
+# print(loggins[0]['login'])
